6a86a1753dc228efaddfd1b779150e25d1cce44e
Eunha <galaxy.eunha.sim@gmail.com>
2024-11-27 16:34:41 -0500

    create a new server

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request,flash
+from flask import Blueprint, render_template, request,flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -28,6 +31,10 @@ def register():
         elif len(password1) <7:
             flash("Password must be at least 7 characters", category="error")
         else:
+            new_user = User(email=email, firstName=firstName, password1=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("You have successfully registered!", category="success")
+            return redirect(url_for('views.home'))
 
     return render_template("register.html")
