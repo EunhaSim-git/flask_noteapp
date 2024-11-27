a1cf1d5b48634c291e8274a27bad6e73e51be6a7
Eunha <galaxy.eunha.sim@gmail.com>
2024-11-27 16:03:47 -0500

    user and note databases created

---
+++ website/models.py
@@ -0,0 +1,17 @@
+from time import timezone
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    userID = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    fistName = db.Column(db.String(120), nullable=False)
+    notes = db.relationship('Note', backref='user', lazy=True)
