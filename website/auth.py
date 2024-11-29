7262e5b1c4005581dfa0f5aa4253079d6f4dada4
Eunha <galaxy.eunha.sim@gmail.com>
2024-11-29 09:58:10 -0500

    only logged in user can access its homepage

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request,flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,6 +16,8 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Wrong password!', category='error')
         else:
@@ -24,9 +27,10 @@ def login():
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -53,6 +57,8 @@ def register():
             db.session.add(new_user)
             db.session.commit()
             flash("You have successfully registered!", category="success")
+            login_user(user, remember=True)
+
             return redirect(url_for('views.home'))
 
     return render_template("register.html")
